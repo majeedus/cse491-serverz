4627c517b0fd8d1b0a94d35be82343e518821425
Usman Majeed <majeedus@msu.edu>
2014-01-16 14:09:33 -0500

    Added HTTP status code and Hello World to server

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import time
 
 s = socket.socket()         # Create a socket object
 host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
+port = 9082
 s.bind((host, port))        # Bind to the port
 
 print 'Starting server on', host, port
@@ -18,6 +18,16 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.recv(1000) # should receive request from client. (GET ....)
+    c.send('HTTP/1.0 200 OK\n')
+    c.send('Content-Type: text/html\n')
+    c.send('\n')
+    c.send("""
+        <html>
+        <body>
+        <h1>Hello, world</h1> this is majeedus's Web server!
+        </body>
+        </html>
+    """)
     c.close()
+	
