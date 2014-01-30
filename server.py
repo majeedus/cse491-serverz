548aa3c88064dee9b96e0d1065386d8f1dc0454c
USM-LINUX <usman@ubuntu.(none)>
2014-01-30 01:51:29 -0500

    Finalized HW2 code

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import time
 
 def main():
 	s = socket.socket()         # Create a socket object
-	host = socket.getfqdn() # Get local machine name
+	host = socket.getfqdn()     # Get local machine name
 	port = random.randint(8000, 9999)
 	s.bind((host, port))        # Bind to the port
 
@@ -19,25 +19,46 @@ def main():
 		# Establish connection with client.    
 		c, (client_host, client_port) = s.accept()
 		print 'Got connection from', client_host, client_port
-		handle_connection(c, host, port)
+		handle_connection(c)
 
+def send200(conn):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n')
+    conn.send('\r\n')
 		
-def handle_connection(conn, host, port):
-	path = conn.recv(1000) # Should receive request from client. (GET ....)
-	path_split = path.split()
-	conn.send('HTTP/1.0 200 OK\r\n')
-	conn.send('Content-Type: text/html\r\n')
-	conn.send('\r\n')
-	conn.send("""
-		<html>
-		<body>
-		<h1>Hello, world.</h1> 
-		This is majeedus\'s Web server.
-		<br/>
-		URL: <a href="">http://%s:%d%s</a>
-		</body>
-		</html>
-	""" % (host, port, path_split[1]))
+def handle_connection(conn):
+	path = conn.recv(1000).splitlines() # Should receive request from client. (GET ....)
+	path_split = path[0].split(' ')
+	
+	send200(conn)
+	
+	if path_split[0] == 'GET':
+		if path_split[1] == '/':
+			conn.send('<html><body>')
+			conn.send('<h1>Welcome to Usman\'s Web Server</h1>')
+			conn.send('<div>')
+			conn.send('<a href="/content">Content</a><br />')
+			conn.send('<a href="/image">Image</a><br />')
+			conn.send('<a href="/file">File</a><br />')
+			conn.send('</div>')
+			conn.send('</body></html>')
+		elif path_split[1] == '/content':
+			conn.send('<html><body>')
+			conn.send('<h1>Content page!</h1>')
+			conn.send('</body></html>')
+		elif path_split[1] == '/file':
+			conn.send('<html><body>')
+			conn.send('<h1>File page!</h1>')
+			conn.send('</body></html>')
+		elif path_split[1] == '/image':
+			conn.send('<html><body>')
+			conn.send('<h1>Image page!</h1>')
+			conn.send('</body></html>')
+	elif path_split[0] == 'POST':
+		conn.send('<html><body>')
+		conn.send('<h1>You have posted content.</h1>')
+		conn.send('</body></html>')
+
 	conn.close()
 	
 if __name__ == '__main__':
--- test_server.py
+++ test_server.py
@@ -1,10 +1,12 @@
+import sys
 import server
+import requests
 
 class FakeConnection(object):
     """
-    A fake connection class that mimics a real TCP socket for the purpose
-    of testing socket I/O.
-    """
+A fake connection class that mimics a real TCP socket for the purpose
+of testing socket I/O.
+"""
     def __init__(self, to_recv):
         self.to_recv = to_recv
         self.sent = ""
@@ -32,8 +34,65 @@ def test_handle_connection():
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
                       'Content-type: text/html\r\n' + \
                       '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is majeedus\'s Web server.'
+                      '<html><body>' + \
+                      '<h1>Welcome to Usman\'s Web Server</h1>' + \
+                      '<div>' + \
+                      '<a href="/content">Content</a><br />' + \
+                      '<a href="/image">Image</a><br />' + \
+                      '<a href="/file">File</a><br />' + \
+                      '</div>' + \
+                      '</body></html>'
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
+def test_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<html><body>' + \
+                      '<h1>Content page!</h1>' + \
+                      '</body></html>'
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<html><body>' + \
+                      '<h1>File page!</h1>' + \
+                      '</body></html>'
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<html><body>' + \
+                      '<h1>Image page!</h1>' + \
+                      '</body></html>'
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_post():
+    conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
+
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                      'Content-type: text/html\r\n' + \
+                      '\r\n' + \
+                      '<html><body>' + \
+                      '<h1>You have posted content.</h1>' + \
+                      '</body></html>'
 
     server.handle_connection(conn)
 
