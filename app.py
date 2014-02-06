d26430631ea596aa468140ccd09eaa3e21e1bad1
majeedus <majeedus@msu.edu>
2014-02-06 18:05:01 -0500

    Finishing up HW5

--- app.py
+++ app.py
@@ -1,22 +1,106 @@
-# from http://docs.python.org/2/library/wsgiref.html
-
 from wsgiref.util import setup_testing_defaults
+import StringIO
+import jinja2
+import urlparse
+import cgi
+import sys
 
-# A relatively simple WSGI application. It's going to print out the
-# environment dictionary after being updated by setup_testing_defaults
-def simple_app(environ, start_response):
-    setup_testing_defaults(environ)
+# Initiate the jinja2 environment and loader
+loader = jinja2.FileSystemLoader('./templates')
+env = jinja2.Environment(autoescape=False, extensions=['jinja2.ext.autoescape'], loader=loader)
 
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
+def app(environ, start_response):
+	setup_testing_defaults(environ)
 
-    start_response(status, headers)
+	status = '200 OK'
+	response_headers = [('Content-type', 'text/html')]
+	response = ''
+	if environ['REQUEST_METHOD'] == 'GET':
+		status, response_headers, response = getRequest(environ)
+	elif environ['REQUEST_METHOD'] == 'POST':
+		status, response_headers, response = postRequest(environ)
+	else:
+		status, response_headers, response = send501(environ)
 
-    ret = ["%s: %s\n" % (key, value)
-           for key, value in environ.iteritems()]
-    ret.insert(0, "This is your environ.  Hello, world!\n\n")
+	start_response(status, response_headers)
+	return response
+	
+def getRequest(environ):	
+    if environ['PATH_INFO'] == '/':
+        return index(environ)
+    elif environ['PATH_INFO'] == '/content':
+        return content(environ)
+    elif environ['PATH_INFO'] == '/file':
+        return file(environ)
+    elif environ['PATH_INFO'] == '/image':
+        return image(environ)
+    elif environ['PATH_INFO'] == '/form':
+        return form(environ)
+    elif environ['PATH_INFO'] == '/formpost':
+        return form_post(environ)
+    elif environ['PATH_INFO'] == '/submit':
+        return get_results(environ)
+    else:
+        return send404(environ)
 
-    return ret
 
-def make_app():
-    return simple_app
+def postRequest(environ):
+	form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+	if environ['PATH_INFO'] == '/submitpost':
+		return post_results(form)
+	else:
+		return send404(environ)
+
+#def send200(conn):
+    #conn.send('HTTP/1.0 200 OK\r\n')
+    #conn.send('Content-type: text/html\r\n')
+    #conn.send('\r\n')
+    
+def send404(environ):
+    return '404 NOT FOUND', [('Content-type', 'text/html')], \
+    env.get_template('error404.html').render({'PATH': environ['PATH_INFO']}).encode('latin-1', 'replace')
+
+def send501(environ):
+    return '404 NOT FOUND', [('Content-type', 'text/html')], \
+    env.get_template('error501.html').render({'PATH': environ['PATH_INFO']}).encode('latin-1', 'replace')
+
+def index(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('index.html').render().encode('latin-1', 'replace')
+
+def content(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('content.html').render().encode('latin-1', 'replace')
+
+def image(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('image.html').render().encode('latin-1', 'replace')
+
+def file(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('file.html').render().encode('latin-1', 'replace')
+
+def form(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('form.html').render().encode('latin-1', 'replace')
+	
+def form_post(environ):
+    return '200 OK', [('Content-type','text/html')], \
+    env.get_template('form_post.html').render().encode('latin-1', 'replace')
+			 
+def get_results(environ):
+    query_string = urlparse.parse_qs(environ['QUERY_STRING'])
+    query = {'firstname':query_string['firstname'][0], 'lastname':query_string['lastname'][0]}
+
+    return '200 OK', [('Content-type', 'text/html')], \
+    env.get_template('form_results.html').render(query).encode('latin-1', 'replace')
+
+def post_results(form):
+    query = {'firstname':form.getvalue('firstname'), 'lastname':form.getvalue('lastname')}
+    
+    return '200 OK', [('Content-type', 'text/html')], \
+    env.get_template('form_results.html').render(query).encode('latin-1', 'replace')
+	
+
+def create_app():
+	return app
--- server.py
+++ server.py
@@ -2,153 +2,91 @@
 import random
 import socket
 import time
-from urlparse import urlparse
-from urlparse import parse_qs
 import cgi
-import StringIO
-import sys
 import jinja2
+import urlparse
+from StringIO import StringIO
+from app import create_app
 
-#Initiate jinja2 library
-loader = jinja2.FileSystemLoader('./templates')
-env = jinja2.Environment(autoescape=False, extensions=['jinja2.ext.autoescape'], loader=loader)
+def handle_connection(conn):
+    info = conn.recv(1)
+
+    # Header Info
+    while info[-4:] != '\r\n\r\n':
+        info += conn.recv(1)
+
+    # Check for POST/GET
+    request = info.split('\r\n')[0].split(' ')[0]
+
+    # Grab the path
+    request_type = info.split('\r\n')[0].split(' ')[1]
+    url_info = urlparse.urlparse(request_type)
+    request_type = url_info.path
+    query = url_info.query
+
+    content       = '';
+    contentType   = '';
+    wsgi_input    = '';
+    content_len = 0;
+    
+    line_split     = info.split('\r\n')
+    if request == 'POST':
+        for s in line_split:
+            if 'Content-Type' in s:
+                contentType = s.split(' ', 1)[1]
+            if 'Content-Length' in s:
+                content_len = int (s.split()[1])
+        for i in range(content_len):
+            content += conn.recv(1)
+        wsgi_input = StringIO(content)
+
+
+    environ = {}
+    environ['requestUEST_METHOD'] = request
+    environ['PATH_INFO']      = request_type
+    environ['QUERY_STRING']   = query
+    environ['CONTENT_TYPE']   = contentType
+    environ['CONTENT_LENGTH'] = content_len
+    environ['wsgi.input']     = wsgi_input
+
+    def start_response(status, response_headers):
+        conn.send('HTTP/1.0')
+        conn.send(status)
+        conn.send('\r\n')
+        for (k,v) in response_headers:
+            conn.send(k)
+            conn.send(v)
+        conn.send('\r\n\r\n')
+
+    wsgi_app = create_app()
+    output   = wsgi_app(environ, start_response)
+    for line in output:
+        conn.send(line)
+    """
+    ret = ["%s: %s\n" % (key, value)
+           for key, value in environ.iteritems()]
+    print ret
+    """
+    conn.close()
 
 def main():
-	s = socket.socket()         # Create a socket object
-	host = socket.getfqdn()     # Get local machine name
-	port = random.randint(8000, 9999)
-	s.bind((host, port))        # Bind to the port
 
-	print 'Starting server on', host, port
-	print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn()     # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
 
-	s.listen(5)                 # Now wait for client connection.
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-	print 'Entering infinite loop; hit CTRL-C to exit'
-	while True:
-		# Establish connection with client.    
-		c, (client_host, client_port) = s.accept()
-		print 'Got connection from', client_host, client_port
-		handle_connection(c)
+    s.listen(5)                 # Now wait for client connection.
 
-def send200(conn):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send('Content-type: text/html\r\n')
-    conn.send('\r\n')
-    
-def send404(conn):
-    conn.send('HTTP/1.0 404 NOT FOUND\r\n')
-    conn.send('Content-type: text/html\r\n')
-    conn.send('\r\n')
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.
+        c, (client_host, client_port) = s.accept()
+        print 'Got connection from', client_host, client_port
+        handle_connection(c)
 
-def send501(conn):
-    conn.send('HTTP/1.0 501 Not Implemented\r\n')
-    conn.send('Content-type: text/html\r\n')
-    conn.send('\r\n')   
-		
-def handle_connection(conn):
-	# Get the request type and folder
-	request_init = ''
-	while True:
-		request_init += conn.recv(1)
-		if '\r\n\r\n' in request_init:
-			break
-		
-	request = StringIO.StringIO(request_init)
-	server = {}
-	server['REQUEST_METHOD'], path, \
-	server['SERVER_PROTOCOL'] = request.readline().split()
-	
-	# Grab the path
-	path = urlparse(path)
-	server['PATH_INFO'] = path.path
-	server['QUERY_STRING'] = path.query
-	
-	# Parse the query string
-	if server['REQUEST_METHOD'] == 'GET':
-		getRequest(conn, server, request)
-	elif server['REQUEST_METHOD'] == 'POST':
-		postRequest(conn, server, request)
-	##else
-		##send500(conn, server)
-	
-	conn.close()
-	
-def getRequest(conn, server, request):	
-	if server['PATH_INFO'] == '/':
-		send200(conn)
-		index(conn, server)
-	elif server['PATH_INFO'] == '/content':
-		send200(conn)
-		content(conn, server)
-	elif server['PATH_INFO'] == '/file':
-		send200(conn)
-		file(conn, server)
-	elif server['PATH_INFO'] == '/image':
-		send200(conn)
-		image(conn, server)
-	elif server['PATH_INFO'] == '/form':
-		send200(conn)
-		form(conn, server)
-	elif server['PATH_INFO'] == '/submit':
-		submit(conn, server)
-	elif server['PATH_INFO'] == '/formpost':
-		send200(conn)
-		form_post(conn, server)
-	else:
-		send404(conn)
-		##error_404(conn, server)
-		
-	conn.close()
-
-def postRequest(conn, server, request):
-    info = {}
-    line = request.readline()
-    while line != '\r\n':
-        k, v = line.split(': ')
-        info[k.lower()] = v.strip('\r\n')
-        line = request.readline()
-
-    if 'content-length' in info.keys():
-        request = StringIO.StringIO(conn.recv(int(info['content-length'])))
-
-    form = cgi.FieldStorage(headers=info, fp=request)
-
-    if server['PATH_INFO'] == '/submitpost':
-        send200(conn)
-        post_results(conn, form)
-    else:
-        send404(conn)
-
-def index(conn, server):
-	conn.send(env.get_template('index.html').render())
-
-def content(conn, server):
-	conn.send(env.get_template('content.html').render())
-
-def image(conn, server):
-	conn.send(env.get_template('image.html').render())
-
-def file(conn, server):
-	conn.send(env.get_template('file.html').render())
-
-def form(conn, server):
-	conn.send(env.get_template('form.html').render())
-	
-def form_post(conn, server):
-	conn.send(env.get_template('form_post.html').render())
-			 
-def submit(conn, server):
-    query_string = parse_qs(server['QUERY_STRING'])
-    vars = {'firstname':query_string['firstname'][0], 'lastname':query_string['lastname'][0]}
-
-    conn.send(env.get_template('form_results.html').render(vars))
-
-def post_results(conn, form):
-	vars = {'firstname':form.getvalue('firstname'), 'lastname':form.getvalue('lastname')}
-    
-	conn.send(env.get_template('form_results.html').render(vars))
-	
-
-if __name__ == '__main__':
-   main()
+if __name__ == "__main__":
+    main()
