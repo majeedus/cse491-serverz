8e22c7396b2bee356f44c6b77c1df14b6b677ace
majeedus <majeedus@msu.edu>
2014-04-27 22:41:15 -0400

    Update for HW13

--- imageapp/__init__.py
+++ imageapp/__init__.py
@@ -1,5 +1,6 @@
 # __init__.py is the top level file in a Python package.
-
+import sqlite3
+import os
 from quixote.publish import Publisher
 
 # this imports the class RootDirectory from the file 'root.py'
@@ -13,7 +14,13 @@ def create_publisher():
  
 def setup():                            # stuff that should be run once.
     html.init_templates()
-
+	#sqllite implementation 
+	if not os.path.exists('./db.sqlite3'):
+		db = sqlite3.connect('db.sqlite3')
+		db.execute('CREATE TABLE IF NOT EXISTS image_store' + 
+					'(i INTEGER PRIMARY KEY, image BLOB, type TEXT,' + 
+	    			'name TEXT, description TEXT)')
+	
     some_data = open('imageapp/dice.png', 'rb').read()
     image.add_image(some_data)
     
